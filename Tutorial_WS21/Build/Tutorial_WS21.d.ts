7e2b450625b6a6e702d578e12edb16a55adf4ac0
Nick Häcker <philipp.haecker@web.de>
2021-11-05 20:19:45 +0100

    delete unnaccessary

--- Tutorial_WS21/Build/Tutorial_WS21.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace Tutorial_WS21 {
-    function Scene1(): ƒS.SceneReturn;
-}
-declare namespace Tutorial_WS21 {
-    export import ƒ = FudgeCore;
-    export import ƒS = FudgeStory;
-}
